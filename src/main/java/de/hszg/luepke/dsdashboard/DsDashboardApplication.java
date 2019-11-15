68b18269bcfac0c98c80157962e9450ee16d9a6b
flikkes <lpke.flx@gmail.com>
2019-11-15 10:54:47 +0100

    initial commit

---
+++ src/main/java/de/hszg/luepke/dsdashboard/DsDashboardApplication.java
@@ -0,0 +1,13 @@
+package de.hszg.luepke.dsdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hszg/luepke/dsdashboard/DsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hszg.luepke.dsdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
